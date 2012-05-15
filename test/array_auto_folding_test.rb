f11745ccdcc7319211c876560c9406fbcb04edf4
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-05-16 00:40:04 +0200

    Test case for https://github.com/jordi/xml-object/issues/4

--- test/array_auto_folding_test.rb
+++ test/array_auto_folding_test.rb
@@ -18,4 +18,20 @@ class ArrayAutoFoldingTest < XMLObject::TestCase
     assert_equal chart.axis, chart.axiss # 's' plural (naïve)
     assert_equal chart.axis, chart.axes if defined?(ActiveSupport::Inflector)
   end
+
+  def test_behaviour_of_arrays_with_zero_and_one_child
+    herd = XMLObject.new('<herd><goats>' + \
+      '<goat>' + \
+        '<eyes><eye>right</eye><eye>left</eye></eyes>' + \
+        '<names></names>' + \
+      '</goat>' + \
+    '</goats></herd>')
+
+    assert_equal 1, herd.goats.length
+
+    goat = herd.goats.first
+    assert_equal 2, goat.eyes.length
+    assert_equal 0, goat.names.length
+    assert_equal 'left', goat.eyes.first
+  end
 end
